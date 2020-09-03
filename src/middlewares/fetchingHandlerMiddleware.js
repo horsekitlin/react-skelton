1a3bc14735bffc32b8d65b9aeeccfdc506ccb021
Tomas Lin <tomas.lin@velocitylight.net>
2020-09-03 15:19:05 +0800

    fetching types 流程調整

--- src/middlewares/fetchingHandlerMiddleware.js
+++ src/middlewares/fetchingHandlerMiddleware.js
@@ -1,13 +1,23 @@
 import types from '../constants/actionTypes';
 import { basicAsyncActionTypes } from '../constants/actionTypes';
 
+const responseRegExp = /_SUCCESS|_ERROR/;
+
 export const startFetchingMiddleware = store => next => action => {
-  const isAsyncAction = basicAsyncActionTypes.includes(action.type);
-  if (isAsyncAction) store.dispatch({ type: types.START_FETCHING });
+  const isBasicAsyncAction = basicAsyncActionTypes.includes(action.type);
+
+  if (isBasicAsyncAction) {
+    store.dispatch({ type: types.START_FETCHING, payload: action.type });
+  }
   return next(action);
 };
 
 export const stopFetchingMiddleware = store => next => action => {
-  if (action['@@redux-saga/SAGA_ACTION']) store.dispatch({ type: types.STOP_FETCHING });
+  const isResponseAction = responseRegExp.test(action.type);
+  const basicType = action.type.replace(responseRegExp, '');
+  const isBasicAsyncAction = basicAsyncActionTypes.includes(basicType);
+  if (isBasicAsyncAction && isResponseAction) {
+    store.dispatch({ type: types.STOP_FETCHING, payload: basicType });
+  }
   return next(action);
 };
--- src/reducers/settingReducer.js
+++ src/reducers/settingReducer.js
@@ -1,12 +1,16 @@
-import {settingState} from './initialState';
-import types from '../constants/actionTypes';
+import { settingState } from './initialState';
+import types from '~/constants/actionTypes';
 
 export default function reducer(setting = settingState, { type, payload }) {
   switch (type) {
-    case types.STOP_FETCHING:
-      return setting.update('fetchCount', count => count -1);
     case types.START_FETCHING:
-      return setting.update('fetchCount', count => count + 1);
+      return setting.update('fetchingTypes', currentValue =>
+        currentValue.add(payload)
+      );
+    case types.STOP_FETCHING:
+      return setting.update('fetchingTypes', currentValue =>
+        currentValue.delete(payload)
+      );
     default:
       return setting;
   }
