cfd82130387db05ca55869d7342ecac930d60e2e
Tomas Lin <thekingofthemountain@gmail.com>
2021-04-09 00:39:15 +0800

    move script path

--- lastest/admin/init_project.sh
+++ lastest/admin/init_project.sh
@@ -4,15 +4,15 @@ npx create-react-app $PROJ_NAME
 cd $PROJ_NAME
 echo '{"compilerOptions": {"baseUrl": "src"},"include": ["src"]}' > jsconfig.json
 rm -rf ./src
-cp -a ../template/. ./src
+cp -a ../template/admin/* ./src
 
-../lastest/add_packages.sh
+../lastest/admin/add_packages.sh
 
 cd ..
 
 rm -rf template lastest README.md 
 
-mv ./$PROJ_NAME/admin/** .
-mv ./$PROJ_NAME/admin/.* .
+mv ./$PROJ_NAME/** .
+mv ./$PROJ_NAME/.* .
 
 rmdir $PROJ_NAME
